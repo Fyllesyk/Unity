c14e48a38a76f4a1b545cff43b5cca8d11987cf1
Andresen <37144320+Fyllesyk@users.noreply.github.com>
2020-02-21 23:57:16 +0100

    Add files via upload

---
+++ Scripts/gun.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+ 
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            transform.Translate(0f, 0.5f, 0f);
+        }
+        else if (Input.GetButtonUp("Fire2"))
+        {
+            transform.Translate(0f, -0.5f, 0f);
+        }
+    }
+}
---
+++ Scripts/shootDemo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootDemo : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public float speed = 100;
+    public int ammoPerMag = 3;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            for (int i = 0; i < ammoPerMag; i++)
+            {
+                Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+
+                instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+                //yield return new WaitForSeconds(1);
+                //StartCoroutine(waitCoroutine());
+            }
+        }
+    }
+    /*IEnumerator waitCoroutine()
+    {
+        yield return new WaitForSeconds(1);
+    }*/
+}
